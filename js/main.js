385229e756f4be3cc16f2aead95a747c1f1e921e
Craig Bartlett <craigbartlett@edgelearningmedia.com>
2020-03-25 08:13:16 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,322 @@
+var data = {
+    "cells": [
+        { "id": 0, value: "s", "meta": 1 },
+        { "id": 1, value: "i", "meta": 0 },
+        { "id": 2, value: "t", "meta": 2 },
+        { "id": 3, value: "e", "meta": 0 },
+        { "id": 4, value: "blank", "meta": 0 },
+        { "id": 5, value: "c", "meta": 3 },
+        { "id": 6, value: "a", "meta": 4 },
+        { "id": 7, value: "n", "meta": 0 },
+        { "id": 8, value: "s", "meta": 5 },
+
+        { "id": 9, value: "u", "meta": 0 },
+        { "id": 10, value: "blank", "meta": 0 },
+        { "id": 11, value: "e", "meta": 0 },
+        { "id": 12, value: "blank", "meta": 0 },
+        { "id": 13, value: "blank", "meta": 0 },
+        { "id": 14, value: "blank", "meta": 0 },
+        { "id": 15, value: "l", "meta": 0 },
+        { "id": 16, value: "blank", "meta": 0 },
+        { "id": 17, value: "e", "meta": 0 },
+
+        { "id": 18, value: "b", "meta": 6 },
+        { "id": 19, value: "e", "meta": 7 },
+        { "id": 20, value: "e", "meta": 0 },
+        { "id": 21, value: "s", "meta": 8 },
+        { "id": 22, value: "blank", "meta": 0 },
+        { "id": 23, value: "s", "meta": 9 },
+        { "id": 24, value: "i", "meta": 0 },
+        { "id": 25, value: "d", "meta": 10 },
+        { "id": 26, value: "e", "meta": 0 },
+
+        { "id": 27, value: "blank", "meta": 0 },
+        { "id": 28, value: "n", "meta": 0 },
+        { "id": 29, value: "blank", "meta": 0 },
+        { "id": 30, value: "a", "meta": 11 },
+        { "id": 31, value: "n", "meta": 0 },
+        { "id": 32, value: "t", "meta": 0 },
+        { "id": 33, value: "blank", "meta": 0 },
+        { "id": 34, value: "r", "meta": 0 },
+        { "id": 35, value: "blank", "meta": 0 },
+
+        { "id": 36, value: "i", "meta": 12 },
+        { "id": 37, value: "d", "meta": 0 },
+        { "id": 38, value: "a", "meta": 0 },
+        { "id": 39, value: "s", "meta": 0 },
+        { "id": 40, value: "blank", "meta": 0 },
+        { "id": 41, value: "a", "meta": 13 },
+        { "id": 42, value: "x", "meta": 0 },
+        { "id": 43, value: "i", "meta": 0 },
+        { "id": 44, value: "s", "meta": 0 },
+
+        { "id": 45, value: "blank", "meta": 1 },
+        { "id": 46, value: "e", "meta": 0 },
+        { "id": 47, value: "blank", "meta": 2 },
+        { "id": 48, value: "s", "meta": 14 },
+        { "id": 49, value: "u", "meta": 0 },
+        { "id": 50, value: "n", "meta": 0 },
+        { "id": 52, value: "blank", "meta": 4 },
+        { "id": 53, value: "v", "meta": 0 },
+        { "id": 54, value: "blank", "meta": 5 },
+
+        { "id": 9, value: "i", "meta": 15 },
+        { "id": 10, value: "d", "meta": 0 },
+        { "id": 11, value: "l", "meta": 16 },
+        { "id": 12, value: "y", "meta": 0 },
+        { "id": 13, value: "blank", "meta": 0 },
+        { "id": 14, value: "d", "meta": 17 },
+        { "id": 15, value: "e", "meta": 18 },
+        { "id": 16, value: "e", "meta": 0 },
+        { "id": 17, value: "r", "meta": 19 },
+
+        { "id": 9, value: "v", "meta": 0 },
+        { "id": 10, value: "blank", "meta": 0 },
+        { "id": 11, value: "o", "meta": 0 },
+        { "id": 12, value: "blank", "meta": 0 },
+        { "id": 13, value: "blank", "meta": 0 },
+        { "id": 14, value: "blank", "meta": 0 },
+        { "id": 15, value: "v", "meta": 0 },
+        { "id": 16, value: "blank", "meta": 0 },
+        { "id": 17, value: "e", "meta": 0 },
+
+        { "id": 9, value: "e", "meta": 20 },
+        { "id": 10, value: "a", "meta": 0 },
+        { "id": 11, value: "s", "meta": 0 },
+        { "id": 12, value: "t", "meta": 0 },
+        { "id": 13, value: "blank", "meta": 0 },
+        { "id": 14, value: "r", "meta": 21 },
+        { "id": 15, value: "e", "meta": 0 },
+        { "id": 16, value: "a", "meta": 0 },
+        { "id": 17, value: "d", "meta": 0 }
+    ],
+    "hints": {
+        "across": [
+            { "hint": "1. Locale" },
+            { "hint": "3. Soup containers" },
+            { "hint": "6. Nectar gatherers" },
+            { "hint": "9. Fries or slaw" },
+            { "hint": "11. Household pest" },
+            { "hint": "12. Belonging to Ida" },
+            { "hint": "13. ___ of Evil" },
+            { "hint": "14. Morning riser" },
+            { "hint": "15. Lazily" },
+            { "hint": "17. Buck or doe" },
+            { "hint": "20. Sunrise direction" },
+            { "hint": "21. You do this with a book" }
+        ],
+        "down": [
+            { "hint": "1. Long sandwich" },
+            { "hint": "2. Casual top" },
+            { "hint": "4. ___ Baba" },
+            { "hint": "5. Observe" },
+            { "hint": "7. Concluded" },
+            { "hint": "8. Cheeky" },
+            { "hint": "9. Don't sit" },
+            { "hint": "10. Tee off" },
+            { "hint": "15. 'If ___ said it once...'" },
+            { "hint": "16. ___ Angeles" },
+            { "hint": "18. New Year's ___" },
+            { "hint": "19. Scarlet or Maroon" },
+        ]
+    },
+    "solutions": [
+        { "word": "site", "arr": [0, 1, 2, 3] },
+        { "word": "cans", "arr": [5, 6, 7, 8] },
+        { "word": "bees", "arr": [18, 19, 20, 21] },
+        { "word": "side", "arr": [23, 24, 25, 26] },
+        { "word": "ant", "arr": [30, 31, 32] },
+        { "word": "idas", "arr": [36, 37, 38, 39] },
+        { "word": "axis", "arr": [41, 42, 43, 44] },
+        { "word": "sun", "arr": [48, 49, 50] },
+        { "word": "idly", "arr": [54, 55, 56, 57] },
+        { "word": "deer", "arr": [59, 60, 61, 62] },
+        { "word": "east", "arr": [72, 73, 74, 75] },
+        { "word": "read", "arr": [77, 78, 79, 80] },
+        { "word": "sub", "arr": [0, 9, 18] },
+        { "word": "tee", "arr": [2, 11, 20] },
+        { "word": "ali", "arr": [6, 15, 24] },
+        { "word": "see", "arr": [8, 17, 26] },
+        { "word": "ended", "arr": [19,28,37,46,55] },
+        { "word": "sassy", "arr": [21,30,39,48,57] },
+        { "word": "stand", "arr": [23,32,41,50,59] },
+        { "word": "drive", "arr": [25,34,43,52,61] },
+        { "word": "ive", "arr": [54,63,72] },
+        { "word": "los", "arr": [56,65,74] },
+        { "word": "eve", "arr": [60,69,78] },
+        { "word": "red", "arr": [62,71,80] },
+    ]
+}
+
+/* 
+    Feedback Styles
+    0 : by letter 
+    1 : By Word
+*/
+
+var feedback_style = 1;
+
+var letterArray = [];
+var userDefinedArray = [];
+
+var iL = data.cells.length;
+
+buildBoard();
+buildHints();
+
+function buildBoard() {
+
+    for (i = 0; i < iL; i++) {
+        var cell = document.createElement('div');
+        var input = document.createElement('input');
+        var number = document.createElement('span');
+
+        input.maxLength = 1;
+        input.id = i;
+
+        if (data.cells[i].value != "blank") {
+            cell.appendChild(input);
+            cell.id = "cell_" + i;
+        }
+
+        document.getElementById('numbers').appendChild(number);
+
+        number.className = 'number'
+        input.className = "input";
+
+        letterArray.push(data.cells[i].value);
+
+        if (data.cells[i].value != "blank") {
+            cell.className = 'letter_cell';
+            userDefinedArray.push("");
+        } else {
+            cell.className = 'blank_cell';
+            userDefinedArray.push("blank");
+        }
+
+        if (data.cells[i].value != "blank") {
+            if (data.cells[i].meta != 0)
+                number.innerHTML = data.cells[i].meta;
+                //number.innerHTML = i;
+
+        }
+        document.getElementById('crossword').appendChild(cell);
+    }
+}
+
+function buildHints() {
+
+    var alist = document.createElement('ul');
+    var iL = data.hints["across"].length;
+    var jL = data.hints["down"].length;
+
+    for (i = 0; i < iL; i++) {
+        var item = document.createElement('li');
+        item.innerHTML = data.hints["across"][i].hint;
+        alist.appendChild(item);
+    }
+    var blist = document.createElement('ul');
+    for (j = 0; j < jL; j++) {
+        var item = document.createElement('li');
+        item.innerHTML = data.hints["down"][j].hint;
+        blist.appendChild(item);
+    }
+
+    document.getElementById("across").appendChild(alist);
+    document.getElementById("down").appendChild(blist);
+}
+
+function evaluateUserArray() {
+    if (feedback_style === 0) {
+
+        for (i = 0; i < iL; i++) {
+            var cell = document.getElementById("cell_" + i);
+            var input = document.getElementById(i);
+
+            if (letterArray[i] === userDefinedArray[i]) {
+                if (letterArray[i] != "blank") {
+                    cell.className = "correct";
+                    input.className = "input_correct";
+                }
+            } else if (letterArray[i] != userDefinedArray[i]) {
+                if (userDefinedArray[i] != "" && letterArray[i] != "blank") {
+                    cell.className = "incorrect";
+                    input.className = "input_incorrect";
+                }
+            }
+        }
+    } else {
+        var jL = data.solutions.length;
+
+        var _isMatch = false;
+        for (j = 0; j < jL; j++) {
+            var kL = data.solutions[j].arr.length;
+            for (k = 0; k < kL; k++) {
+                index = data.solutions[j].arr[k];
+                if (userDefinedArray[index] != data.solutions[j].word[k]) {
+                    _isMatch = false;
+                    break;
+                } else {
+                    _isMatch = true;
+                }
+            }
+
+            for (l = 0; l < kL; l++) {
+                index = data.solutions[j].arr[l];
+                var cell = document.getElementById("cell_" + index);
+                var input = document.getElementById(index);
+
+                if (_isMatch) {
+                    cell.className = "correct";
+                    input.className = "input_correct";
+                } else if(!_isMatch){
+                    if (userDefinedArray[l] != "" && letterArray[l] != "blank") {
+                        cell.className = "incorrect";
+                        input.className = "input_incorrect";
+                    }
+                }
+            }
+        }
+    }
+}
+
+function calculateRemaining(b) {
+    var _diff = 0;
+    var _remaining = 0;
+
+    var iL = b.length;
+    for(i=0;i<iL;i++){
+        if(b[i] === "")
+            _remaining++;
+    }
+    
+    if(_remaining > 0)
+    {
+        //complete = false;
+        document.getElementById('finish_button').innerHTML = "Remaining " + _remaining;
+    }
+    else{
+        //complete = true
+        document.getElementById('finish_button').classList.remove("disabled");
+        document.getElementById('finish_button').innerHTML = "Click to Complete";
+    } 
+
+    console.log("Remaining : " + _remaining);
+}
+
+document.onkeyup = function (evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+
+    var input = document.getElementById(document.activeElement.id);
+
+    input.classList.remove("input_incorrect");
+    input.className = 'input_reset';
+    input.parentElement.classList.remove("incorrect");
+    input.parentElement.className = 'reset';
+
+    userDefinedArray[document.activeElement.id] = input.value;
+    //if(!complete)
+        calculateRemaining(userDefinedArray);
+};
